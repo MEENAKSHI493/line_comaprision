3b4c145f65800bae4f70db9d8e98b2bee00a6d59
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-23 01:51:22 +0530

    line comparision

--- src/javalinecomparision/linecomparision.java
+++ src/javalinecomparision/linecomparision.java
@@ -9,6 +9,4 @@ public class linecomparision {
 	    dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
             System.out.println("Length of line of coordinates"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+dis);
 	}
-
-	}
-
+}
